210b23c6291b18d422f06155817c97868b62eb85
glowsoony <chrisalex08reyes@gmail.com>
2023-07-04 02:49:03 -0400

    Missed my texture (my love) 💕

--- source/Paths.hx
+++ source/Paths.hx
@@ -20,6 +20,7 @@ import sys.FileSystem;
 #end
 import flixel.graphics.FlxGraphic;
 import openfl.display.BitmapData;
+import openfl.display3D.textures.Texture; // GPU STUFF
 import haxe.Json;
 
 import flash.media.Sound;
