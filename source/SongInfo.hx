c6f5cbd47b7405c138b3842f308f9caea941c9b1
DrkFon376 <lvrderek@gmail.com>
2024-06-01 23:47:39 -0600

    New lua support for customizing the text, the jukeBoxTag color and the ability to disable it

--- source/SongInfo.hx
+++ source/SongInfo.hx
@@ -20,16 +20,24 @@ class SongInfo extends FlxSpriteGroup
     var songNameTextSize:Int = 30;
     var boxTagWidth:Int = 15;
 
-    // Inspired by the TaskSong from VS Impostor v4!
+    public static var daAuthorInfo:String = ""; //Customizable using lua!
+    public static var customJukeBoxTagColor:String = ""; //This too!
+    public static var disabled:Bool = false; //You can also disable it using lua
+
+    //Inspired by the TaskSong from VS Impostor v4!
 
     public function new(x:Float, y:Float, song:String, authorInfo:String = "") {
 
         super(x, y);
+
+        var colorNum:Int = Std.parseInt(customJukeBoxTagColor);
+        if(customJukeBoxTagColor != "" && !customJukeBoxTagColor.startsWith('0x'))
+            colorNum = Std.parseInt('0xFF' + customJukeBoxTagColor);
         
         var jukeBox = new FlxSprite(0 - boxTagWidth * 2, 15).makeGraphic(300 + boxTagWidth, 160, FlxColor.BLACK);
         jukeBox.alpha = 0.5;
 
-        var jukeBoxTag = new FlxSprite(0 - boxTagWidth * 2 + jukeBox.width, 15).makeGraphic(20, 160, 0xFF3EFFE4);
+        var jukeBoxTag = new FlxSprite(0 - boxTagWidth * 2 + jukeBox.width, 15).makeGraphic(20, 160, (customJukeBoxTagColor != "" ? colorNum : FlxColor.fromRGB(PlayState.instance.dad.healthColorArray[0], PlayState.instance.dad.healthColorArray[1], PlayState.instance.dad.healthColorArray[2])));
         jukeBoxTag.alpha = 0.5;
 
         var jukeBoxText = new FlxText(0 - boxTagWidth, 30, 300, "Now playing:", fontSize);
@@ -40,7 +48,7 @@ class SongInfo extends FlxSpriteGroup
         jukeBoxSongName.setFormat(Paths.font("PhantomMuff Full Letters 1.1.5.ttf"), songNameTextSize, FlxColor.WHITE, FlxTextAlign.LEFT, FlxTextBorderStyle.OUTLINE, FlxColor.BLACK);
         jukeBoxSongName.updateHitbox();
 
-        var authorInfoText = new FlxText(0 - boxTagWidth, 100, 300, "Song by: " + authorInfo, fontSize);
+        var authorInfoText = new FlxText(0 - boxTagWidth, 100, 300, (daAuthorInfo != "" ? daAuthorInfo : authorInfo), fontSize);
         authorInfoText.setFormat(Paths.font("PhantomMuff Full Letters 1.1.5.ttf"), fontSize, FlxColor.WHITE, FlxTextAlign.LEFT, FlxTextBorderStyle.OUTLINE, FlxColor.BLACK);
         authorInfoText.updateHitbox();
 
@@ -54,13 +62,16 @@ class SongInfo extends FlxSpriteGroup
     }
 
     public function start(){
-        alpha = 1;
+        if (!disabled)
+        {
+            alpha = 1;
 
-        // Was a great idea to incorporate the tweens to the modchartTweens map of PlayState :mirada_sexy:
-        PlayState.instance.modchartTweens.set("SongInfoShit", FlxTween.tween(this, {x: 25}, 1.5, {ease: FlxEase.quintInOut, onComplete: function(twn:FlxTween){
-            PlayState.instance.modchartTweens.set("SongInfoShit2", FlxTween.tween(this, {x: -360}, 1.5, {ease: FlxEase.quintInOut, startDelay: 2.5, onComplete: function(twn:FlxTween){ 
-                this.destroy(); 
+            //Was a great idea to incorporate the tweens to the modchartTweens map of PlayState :mirada_sexy:
+            PlayState.instance.modchartTweens.set("SongInfoShit", FlxTween.tween(this, {x: 25}, 1.5, {ease: FlxEase.quintInOut, onComplete: function(twn:FlxTween){
+                PlayState.instance.modchartTweens.set("SongInfoShit2", FlxTween.tween(this, {x: -360}, 1.5, {ease: FlxEase.quintInOut, startDelay: 2.5, onComplete: function(twn:FlxTween){ 
+                    this.destroy(); 
+                }}));
             }}));
-        }}));
+        }
     }
 }
