846e0e86a82e84719e310710f013c64024793688
DrkFon376 <lvrderek@gmail.com>
2024-07-13 22:59:52 -0600

    Stages updated and renamed, song jsons also updated

--- assets/preload/stages/Stage-Rami-Changer.lua
+++ assets/preload/stages/Stage-Rami-Changer.lua
@@ -5,20 +5,20 @@ function onCreate()
 	setLuaSpriteScrollFactor('stageback', 1.0, 1.0);
 	scaleObject('stageback', 1.0, 1.0);
 
-	makeLuaSprite('stagefront', 'StageHotfix/StageFront', -201, 1048);
+	makeLuaSprite('stagefront', 'StageHotfix/StageFront', -208, 1031);
 	setLuaSpriteScrollFactor('stagefront', 1.0, 1.0);
 	scaleObject('stagefront', 1.0, 1.0);
 
-    makeLuaSprite('mueble', 'StageHotfix/StageMueble', 575, 602);
+    makeLuaSprite('mueble', 'StageHotfix/StageMueble', 569, 607);
     setLuaSpriteScrollFactor('mueble', 1.0, 1.0);
     scaleObject('mueble', 1.1, 1.1);
 
     if not lowQuality then
-	    makeLuaSprite('adornos', 'StageHotfix/Adornos', -110, 295);
+	    makeLuaSprite('adornos', 'StageHotfix/Adornos', -110, 305);
 	    setLuaSpriteScrollFactor('adornos', 1.0, 1.0);
 	    scaleObject('adornos', 1.0, 1.0);
 
-		makeLuaSprite('extra', 'StageHotfix/StageExtra2', 275, 715);
+		makeLuaSprite('extra', 'StageHotfix/StageExtra2', 290, 715);
 		setLuaSpriteScrollFactor('extra', 1.0, 1.0);
 		scaleObject('extra', 1.0, 1.0);
 
@@ -27,7 +27,7 @@ function onCreate()
 		scaleObject('luz', 1.0, 1.0);
 		setProperty('luz.alpha', 0.2);
     end
-
+	
 	--old stage assets
 	makeLuaSprite('stagebackold', 'StageHotfix/StageBackOLD', -200, 0);
 	setLuaSpriteScrollFactor('stagebackold', 1.0, 1.0);
--- assets/preload/stages/Stage-Rami.lua
+++ assets/preload/stages/Stage-Rami.lua
@@ -1,28 +1,28 @@
 --Stage hecha por zRamirez
 function onCreate()
 	--Partes del stage
-	makeLuaSprite('stageback', 'StageZ/StageBack', -200, 0);
+	makeLuaSprite('stageback', 'StageHotfix/StageBack', -200, 0);
 	setLuaSpriteScrollFactor('stageback', 1.0, 1.0);
 	scaleObject('stageback', 1.0, 1.0);
 
-	makeLuaSprite('stagefront', 'StageZ/StageFront', -200, 1050);
+	makeLuaSprite('stagefront', 'StageHotfix/StageFront', -208, 1031);
 	setLuaSpriteScrollFactor('stagefront', 1.0, 1.0);
 	scaleObject('stagefront', 1.0, 1.0);
 
-    makeLuaSprite('mueble', 'StageZ/StageMueble', 586, 699);
+    makeLuaSprite('mueble', 'StageHotfix/StageMueble', 569, 607);
     setLuaSpriteScrollFactor('mueble', 1.0, 1.0);
-    scaleObject('mueble', 1.0, 1.0);
+    scaleObject('mueble', 1.1, 1.1);
 
     if not lowQuality then
-	    makeLuaSprite('adornos', 'StageZ/Adornos', -110, 300);
+	    makeLuaSprite('adornos', 'StageHotfix/Adornos', -110, 305);
 	    setLuaSpriteScrollFactor('adornos', 1.0, 1.0);
 	    scaleObject('adornos', 1.0, 1.0);
 
-		makeLuaSprite('extra', 'StageZ/StageExtra2', 265, 735);
+		makeLuaSprite('extra', 'StageHotfix/StageExtra2', 290, 715);
 		setLuaSpriteScrollFactor('extra', 1.0, 1.0);
 		scaleObject('extra', 1.0, 1.0);
 
-		makeLuaSprite('luz', 'StageZ/idk', -200, 0);
+		makeLuaSprite('luz', 'StageHotfix/idk', -200, 0);
 		setLuaSpriteScrollFactor('luz', 1.0, 1.0);
 		scaleObject('luz', 1.0, 1.0);
 		setProperty('luz.alpha', 0.2);
@@ -34,6 +34,4 @@ function onCreate()
 	addLuaSprite('adornos', false);
 	addLuaSprite('extra', false);
 	addLuaSprite('luz', true);
-
-	close(true);
 end
