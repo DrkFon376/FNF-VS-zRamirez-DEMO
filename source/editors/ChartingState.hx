fa7f51b992bc65d482b5f46d75a00a1dec734efc
zRamírez <VirgateSnow3414@gmail.com>
2025-04-19 22:10:03 -0600

    Bad Battle Fucked terminado al 100% y arregle el path de los eventos del source (gracias por borrarmelo de a gratis glow)

--- source/editors/ChartingState.hx
+++ source/editors/ChartingState.hx
@@ -1077,6 +1077,7 @@ class ChartingState extends MusicBeatState
 		var directories:Array<String> = [Paths.getPreloadPath('custom_notetypes/')];
 
 		#if MODS_ALLOWED
+		directories.push(Paths.getPreloadPath('events/'));
 		directories.push(Paths.mods('custom_notetypes/'));
 		directories.push(Paths.mods(Paths.currentModDirectory + '/custom_notetypes/'));
 		for(mod in Paths.getGlobalMods())
