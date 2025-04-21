8307021393bf76780200cbf540b5c49507c34cc6
zRamírez <VirgateSnow3414@gmail.com>
2025-04-21 08:44:53 -0600

    ahora ya es la version 0.4 oficial

--- source/MainMenuState.hx
+++ source/MainMenuState.hx
@@ -26,7 +26,7 @@ using StringTools;
 class MainMenuState extends MusicBeatState
 {
 	public static var psychEngineVersion:String = '0.6.3h - Custom Build'; //This is also used for Discord RPC
-	public static var vszRamirezVersion:String = 'Demo 3 (v0.4 - BETA!)';
+	public static var vszRamirezVersion:String = 'Demo 3 (v0.4.0)';
 	public static var curSelected:Int = 0;
 
 	var menuItems:FlxTypedGroup<FlxSprite>;
