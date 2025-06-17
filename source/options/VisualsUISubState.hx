ce90d9b1d314dc1f5570e5440a8452aef2c99687
zRamírez <VirgateSnow3414@gmail.com>
2025-06-17 15:30:46 -0600

    MICAMBIOQUEESTABUGPQESUNAPUTAMIERDA

--- source/options/VisualsUISubState.hx
+++ source/options/VisualsUISubState.hx
@@ -24,26 +24,46 @@ import flixel.util.FlxTimer;
 import flixel.input.keyboard.FlxKey;
 import flixel.graphics.FlxGraphic;
 import Controls;
+import Note;
+import StrumNote;
+import NoteSplash;
 
 using StringTools;
 
 class VisualsUISubState extends BaseOptionsMenu
 {
+	var noteOptionID:Int = -1;
+	var notes:FlxTypedGroup<StrumNote>;
+	var notesTween:Array<FlxTween> = [];
+	var noteY:Float = 90;
 	public function new()
 	{
 		title = 'Visuals and UI';
 		rpcTitle = 'Visuals & UI Settings Menu'; //for Discord Rich Presence
 		FlxG.save.data.enteredVisualsOptions = true;
 
-		var option:Option = new Option('Note Skins:',
+		// for note skins
+		notes = new FlxTypedGroup<StrumNote>();
+		for (i in 0...Note.colArray.length)
+		{
+			var note:StrumNote = new StrumNote(370 + (560 / Note.colArray.length) * i, -200, i, 0);
+			note.centerOffsets();
+			note.centerOrigin();
+			note.playAnim('static');
+			notes.add(note);
+		}
+
+		var option:Option = new Option('Note Skin Texture:',
 			"Choose what texture you want to be used in on the Notes.",
 			'noteSkin',
 			'string',
 			'Vanilla',
 			['Vanilla', 'Future', 'Chip', 'Bar', 'Diamond', 'Square', 'Camellia', 'StepMania', 'FutureALT', 'DoritosPizzerola']);
 		addOption(option);
+		option.onChange = onChangeNoteSkin;
+		noteOptionID = optionsArray.length - 1;
 
-		var option:Option = new Option('Note Splashes:',
+		var option:Option = new Option('Note Splashes Texture:',
 			"Choose what texture you want to be used in the Note Splashes.",
 			'noteSplashesTexture',
 			'string',
@@ -192,6 +212,23 @@ class VisualsUISubState extends BaseOptionsMenu
 		super();
 	}
 
+	override function changeSelection(change:Int = 0)
+	{
+		super.changeSelection(change);
+		
+		if(noteOptionID < 0) return;
+
+		for (i in 0...Note.colArray.length)
+		{
+			var note:StrumNote = notes.members[i];
+			if(notesTween[i] != null) notesTween[i].cancel();
+			if(curSelected == noteOptionID)
+				notesTween[i] = FlxTween.tween(note, {y: noteY}, Math.abs(note.y / (200 + noteY)) / 3, {ease: FlxEase.quadInOut});
+			else
+				notesTween[i] = FlxTween.tween(note, {y: -200}, Math.abs(note.y / (200 + noteY)) / 3, {ease: FlxEase.quadInOut});
+		}
+	}
+
 	var changedMusic:Bool = false;
 	function onChangePauseMusic()
 	{
@@ -201,6 +238,25 @@ class VisualsUISubState extends BaseOptionsMenu
 			FlxG.sound.playMusic(Paths.music(Paths.formatToSongPath(ClientPrefs.pauseMusic == 'Breakfast' ? "breakfast-pixel" : (ClientPrefs.pauseMusic == 'zRamírez' ? "ramirez-week-pause" : ClientPrefs.pauseMusic))));
 
 		changedMusic = true;
+	}
+
+	function onChangeNoteSkin()
+	{
+		notes.forEachAlive(function(note:StrumNote) {
+			changeNoteSkin(note);
+			note.centerOffsets();
+			note.centerOrigin();
+		});
+	}
+
+	function changeNoteSkin(note:StrumNote)
+	{
+		var skin:String = 'noteShit/' + ClientPrefs.noteSkin;
+		var customSkin:String = 'noteShit/' + ClientPrefs.noteSkin;
+
+		note.texture = skin; //Load texture and anims
+		note.reloadNote();
+		note.playAnim('static');
 	}
 
 	override function destroy()
