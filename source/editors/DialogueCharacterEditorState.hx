610ba22e4a5186dee51b5c4c8d66c49030b3b41f
glowsoony <84847356+glowsoony@users.noreply.github.com>
2025-06-29 12:56:42 -0400

    Update DialogueCharacterEditorState.hx

--- source/editors/DialogueCharacterEditorState.hx
+++ source/editors/DialogueCharacterEditorState.hx
@@ -78,7 +78,8 @@ class DialogueCharacterEditorState extends MusicBeatState
 
 		if (ClientPrefs.pauseMusic != 'None') 
 		{
-			FlxG.sound.playMusic(Paths.music(ClientPrefs.pauseMusic == 'Breakfast' ? "breakfast-pixel" : (ClientPrefs.pauseMusic == 'zRamírez' ? "ramirez-week-pause" : ClientPrefs.pauseMusic == 'Tea Time' ? "tea-time" : ClientPrefs.pauseMusic)), 0.5);
+			FlxG.sound.playMusic(Paths.music(Paths.formatToSongPath(ClientPrefs.pauseMusic == 'Breakfast' ? "breakfast-pixel" : (ClientPrefs.pauseMusic == 'zRamírez' ? "ramirez-week-pause" : ClientPrefs.pauseMusic == 'Tea Time' ? "tea-time" : ClientPrefs.pauseMusic))), 0.5);
+			trace('Now Playing:'+ ClientPrefs.pauseMusic);
 		}
 		
 		persistentUpdate = persistentDraw = true;
