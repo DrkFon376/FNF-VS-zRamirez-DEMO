ae6f5046666db3a4fa088f26cb593b25409b95b9
glowsoony <84847356+glowsoony@users.noreply.github.com>
2025-06-29 12:56:02 -0400

    Primero Ver

--- source/editors/MenuCharacterEditorState.hx
+++ source/editors/MenuCharacterEditorState.hx
@@ -43,7 +43,7 @@ class MenuCharacterEditorState extends MusicBeatState
 
 		if (ClientPrefs.pauseMusic != 'None') 
 		{
-			FlxG.sound.playMusic(Paths.music(ClientPrefs.pauseMusic == 'Breakfast' ? "breakfast-pixel" : (ClientPrefs.pauseMusic == 'zRamírez' ? "ramirez-week-pause" : ClientPrefs.pauseMusic == 'Tea Time' ? "tea-time" : ClientPrefs.pauseMusic)), 0.5);
+			FlxG.sound.playMusic(Paths.music(Paths.formatToSongPath(ClientPrefs.pauseMusic == 'Breakfast' ? "breakfast-pixel" : (ClientPrefs.pauseMusic == 'zRamírez' ? "ramirez-week-pause" : ClientPrefs.pauseMusic == 'Tea Time' ? "tea-time" : ClientPrefs.pauseMusic))), 0.5);
 			trace('Now Playing:'+ ClientPrefs.pauseMusic);
 		}
 
